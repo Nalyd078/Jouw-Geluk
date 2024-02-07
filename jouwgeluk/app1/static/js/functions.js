de119080867626c6b1bf6a19e41f43dde0c686b3
Nalyd078 <147247441+Nalyd078@users.noreply.github.com>
2024-02-07 07:25:26 +0100

    Add files via upload

--- jouwgeluk/app1/static/js/functions.js
+++ jouwgeluk/app1/static/js/functions.js
@@ -19,3 +19,20 @@ function change_route(route, script, scripttag) {
     })
   },200);
 }
+
+ // WHATSAPP ICOON
+
+ $(document).ready(function() {
+   $('.whatsapp-button').hover(
+     function() { // Mouse enter of touch
+       $(this).stop().animate({ width: 'auto' }, 300); // Breidt uit om de tekst te tonen
+       $('span', this).css({ opacity: '1', transform: 'translateX(0px)' });
+     },
+     function() { // Mouse leave
+       if (!$(this).is(':active')) {
+         $(this).stop().animate({ width: '35px' }, 300); // Krimp naar de breedte van alleen het icoon
+         $('span', this).css({ opacity: '0', transform: 'translateX(20px)' });
+       }
+     }
+   );
+ });
